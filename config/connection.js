5a1624a52fa56849f6300862abd0115a4f0cace9
Wendy Tekverk <wtekverk@gmail.com>
2021-03-23 13:20:06 -0600

    adding comments to conection js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
-// Set up MySQL connection.
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'cat_db',
-});
+var mysql = require("mysql");
 
-// Make connection.
-connection.connect((err) => {
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+
+    //Make sure you update your own password here! 
+    password: "HobGoblin93",
+    //db name 
+    database: "burger_db",
+  });
+}
+
+//setting up connection 
+connection.connect(function (err) {
   if (err) {
-    console.error(`error connecting: ${err.stack}`);
+
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log(`connected as id ${connection.threadId}`);
+
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
