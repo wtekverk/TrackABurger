469d4730179b38736d28296e5a6b58067c67e3b4
Wendy Tekverk <wtekverk@gmail.com>
2021-03-25 13:30:50 -0600

    updating seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES (Whopper, false)
+
 INSERT INTO burgers (burger_name, devoured) VALUES (Big Mac, false)
+INSERT INTO burgers (burger_name, devoured) VALUES (Whopper, false)
 INSERT INTO burgers (burger_name, devoured) VALUES (Thickburger, false)
