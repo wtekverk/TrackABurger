2f1695174204422a20d7724f4b292196d101a0b5
Wendy Tekverk <wtekverk@gmail.com>
2021-03-26 10:58:04 -0600

    changing format of burger controlller js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,28 +8,28 @@ const burger = require('../models/burger.js');
 router.get('/', (req, res) => {
   burger.selectAll((data) => {
     const hbsObject = {
-      cats: data,
+      burgers: data,
     };
     console.log(hbsObject);
     res.render('index', hbsObject);
   });
 });
 
-router.post('/api/cats', (req, res) => {
-  cat.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], (result) => {
+router.post('/api/burgers', (req, res) => {
+  burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put('/api/cats/:id', (req, res) => {
+router.put('/api/burgers/:id', (req, res) => {
   const condition = `id = ${req.params.id}`;
 
   console.log('condition', condition);
 
-  cat.update(
+  burger.updateOne(
     {
-      sleepy: req.body.sleepy,
+      devoured: req.body.devoured,
     },
     condition,
     (result) => {
@@ -42,17 +42,6 @@ router.put('/api/cats/:id', (req, res) => {
   );
 });
 
-router.delete('/api/cats/:id', (req, res) => {
-  const condition = `id = ${req.params.id}`;
-
-  cat.delete(condition, (result) => {
-    if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
