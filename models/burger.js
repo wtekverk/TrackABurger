00ac480ef230a69920e3506c416784122f95dda2
Wendy Tekverk <wtekverk@gmail.com>
2021-03-28 15:54:22 -0600

    reformating berger js

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,19 @@
 const orm = require('../config/orm.js');
 
 const burger = {
+  
   select(cb) {
 
     orm.select('burgers', (res) => cb(res));
   },
+
   insert(cols, vals, cb) {
+
     orm.insert('burgers', cols, vals, (res) => cb(res));
   },
+
   update(objColVals, condition, cb) {
+
     orm.update('burgers', objColVals, condition, (res) => cb(res));
   },
   
