0523150eb0e6cf2f812ddd9d25377f71b3d765c2
Wendy Tekverk <wtekverk@gmail.com>
2021-03-28 14:56:21 -0600

    fixing typo in burger js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -49,13 +49,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
       e.preventDefault();
 
       // Grabs the value of the textarea that goes by the name, "quote"
-      const newCat = {
-        name: document.getElementById('ca').value.trim(),
-        sleepy: document.getElementById('sleepy').checked,
+      const newBurger = {
+        burger_name: document.getElementById('ca').value.trim(),
+        devoured: false,
       };
 
       // Send POST request to create a new quote
-      fetch('/api/cats', {
+      fetch('/api/burgers', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
@@ -63,36 +63,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         },
 
         // make sure to serialize the JSON body
-        body: JSON.stringify(newCat),
+        body: JSON.stringify(newBurger),
       }).then(() => {
         // Empty the form
         document.getElementById('ca').value = '';
 
         // Reload the page so the user can see the new quote
-        console.log('Created a new cat!');
+        console.log('Created a new burger!');
         location.reload();
       });
     });
   }
 
-  // DELETE
-  const deleteCatBtns = document.querySelectorAll('.delete-cat');
-
-  // Set up the event listeners for each delete button
-  deleteCatBtns.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
-
-      // Send the delete request
-      fetch(`/api/cats/${id}`, {
-        method: 'DELETE',
-      }).then((res) => {
-        console.log(res);
-        console.log(`Deleted cat: ${id}`);
-
-        // Reload the page
-        location.reload();
-      });
-    });
-  });
 });
