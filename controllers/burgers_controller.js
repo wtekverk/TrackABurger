6a070400b780498f121244dbc6bc30ce7507fff0
Wendy Tekverk <wtekverk@gmail.com>
2021-03-28 15:53:34 -0600

    reformating bers controler js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+
 const burger = require('../models/burger.js');
+
 
 router.get('/',(req,res)=> {
     burger.select((data)=>{
         const hbsObject = {
             burgers: data,
         };
+
         console.log(hbsObject);
         res.render('index',hbsObject);
     });
 });
 
 router.post('/api/burgers', (req,res) =>{
- console.log(req.body);
+ 
 
     burger.insert(['burger_name','devoured'],[req.body.burger_name, req.body.devoured], (data)=> {
         res.json({id: data.insertId});
@@ -26,6 +29,7 @@ router.put('/api/burgers/:id',(req,res)=>{
   const condition =  `id = ${req.params.id}`;
 
     burger.update(
+        
          "devoured = "+req.body.devoured,
           condition,
         (data)=>{
