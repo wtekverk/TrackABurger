5d305ded0d0ecc24e0b37ffb39c36645fc0d3bfd
Wendy Tekverk <wtekverk@gmail.com>
2021-03-28 15:17:13 -0600

    deleting delete functionality from burger js in models

--- models/burger.js
+++ models/burger.js
@@ -3,6 +3,7 @@ const orm = require('../config/orm.js');
 
 const burger = {
   all(cb) {
+
     orm.all('burgers', (res) => cb(res));
   },
   // The variables cols and vals are arrays.
@@ -12,9 +13,7 @@ const burger = {
   update(objColVals, condition, cb) {
     orm.update('burgers', objColVals, condition, (res) => cb(res));
   },
-  delete(condition, cb) {
-    orm.delete('burgers', condition, (res) => cb(res));
-  },
+  
 };
 
 // Export the database functions for the controller (catsController.js).
